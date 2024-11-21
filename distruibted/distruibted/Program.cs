5426c098adae17b9b438c35bacdcab499167dbac
Jaafarsafieddine <202010750@ua.edu.lb>
2024-11-22 00:28:38 +0200

    Added greeting API endpoint to the Weather App

--- distruibted/distruibted/Program.cs
+++ distruibted/distruibted/Program.cs
@@ -1,7 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -20,6 +19,15 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.MapControllers();
+
+// New Endpoint: Greeting API
+app.MapGet("/api/greeting", () =>
+{
+    return Results.Ok(new { Message = "Hello! Welcome to the enhanced Weather App!" });
+});
+
+// Existing Controllers Mapping
 app.MapControllers();
 
 app.Run();
